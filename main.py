0530c1ccdc5a591c94707a27032bc632475e8eb6
Glazkova_Marina <glazkova.mv@bk.ru>
2023-08-09 13:50:55 +0300

    createNode v1

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-note = {
-    'id': 0,
-    'name': 'New note',
-    'body': 'test note',
-    'date': '05.05.2023',
-    'time': '12:35'
-}
+from datetime import datetime
 
-print(note.get('date'));
 
 def createNode():
-    print("Введите название: ")
+    note = { 'id': 0, 'name': 'New note', 'body': 'test note', 'date': '1.1.2023', 'time': '00:00'
+}
+    note['id'] = 0
+    note['name'] = input("Введите название: ")
+    note['body'] = input("Введите текст заметки: ")
+    now = datetime.now()
+    note['date'] = "{}.{}.{}".format(now.day, now.month, now.year)
+    note['time'] = "{}:{}".format(now.hour, now.minute)
+    print(note)
+    return note
+
+createNode()
