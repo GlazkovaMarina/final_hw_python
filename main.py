fc01bfb0a38e493e512badf2cea5cc4db5b84151
Glazkova_Marina <glazkova.mv@bk.ru>
2023-08-09 15:57:54 +0300

    some changes

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import json
 import os
 
 def readNotes():
-    try:
-        with open('notes.json', 'r') as file:
-            data = json.load(file)
-            print(data)
-    except OSError:
-        print("Произошла ошибка при работе с файлом:(")
+    if os.path.exists("notes.json"):
+        try:
+            with open('notes.json', 'r') as file:
+                data = json.load(file)
+                for i in data['note']:
+                    print(i)
+        except OSError:
+            print("\nПроизошла ошибка при работе с файлом:(")
+    else:
+        print("\nЗаметок пока еще не создавалось!\n")
 def saveNote(note):
     if os.path.exists("notes.json"):
         try:
@@ -19,9 +23,8 @@ def saveNote(note):
             with open('notes.json', 'w') as file:
                 json.dump(data, file, indent='\t', ensure_ascii=False)
         except OSError:
-            print("Произошла ошибка при работе с файлом:(")
+            print("\nПроизошла ошибка при работе с файлом:(\n")
     else:
-        print("Файл не найден")
         try:
             with open('notes.json', 'w') as file:
                 data = {}
@@ -29,7 +32,7 @@ def saveNote(note):
                 data['note'].append(note)
                 json.dump(data, file, indent='\t', ensure_ascii=False)
         except OSError:
-            print("Произошла ошибка при работе с файлом:(")
+            print("\nПроизошла ошибка при работе с файлом:(\n")
 
 
 def createNote():
@@ -42,7 +45,7 @@ def createNote():
                 data = json.load(file)
                 id = len(data['note'])
         except OSError:
-            print("Произошла ошибка при работе с файлом при считывании id заметок!")
+            print("\nПроизошла ошибка при работе с файлом при считывании id заметок!\n")
     note['id'] = id
     note['name'] = input("Введите название: ")
     note['body'] = input("Введите текст заметки: ")
@@ -53,9 +56,9 @@ def createNote():
     choise = 3
     while (choise != 2):
         try:
-            choise = int(input('Если хотите сохранить данную заметку, то нажмите 1, иначе 2: '))
+            choise = int(input('\nЕсли хотите сохранить данную заметку, то нажмите 1, иначе 2: '))
         except ValueError:
-            print('Ошибка ввода. Необходимо ввести число 1 или 2!\n')
+            print('\nОшибка ввода. Необходимо ввести число 1 или 2!\n')
         if choise == 1:
             saveNote(note)
             choise = 2
@@ -64,11 +67,11 @@ def main():
     choise = 0
     while choise != 6:
         try:
-            choise = int(input("Выберите действие, нажав соответствующую цифру:\n1 - создание новой заметки,\n2 - редактирование заметки,\n3 - удаление заметки,\n4 - просмотр всех заметок,\n5 - просмотр заметки по выбранной дате\n6 - завершение работы с заметками: "))
+            choise = int(input("\nВыберите действие, нажав соответствующую цифру:\n1 - создание новой заметки,\n2 - редактирование заметки,\n3 - удаление заметки,\n4 - просмотр всех заметок,\n5 - просмотр заметки по выбранной дате\n6 - завершение работы с заметками: "))
             if choise < 1 or choise > 6:
-                print('Ошибка ввода. Необходимо ввести число от 1 до 6!\n')
+                print('\nОшибка ввода. Необходимо ввести число от 1 до 6!\n')
         except ValueError:
-            print('Ошибка ввода. Необходимо ввести число от 1 до 6!\n')
+            print('\nОшибка ввода. Необходимо ввести число от 1 до 6!\n')
         if choise == 1:
             createNote()
         elif choise == 4:
