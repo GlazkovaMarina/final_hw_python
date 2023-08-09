d2111de3e8553be891bc8e3817277dd96b8d7ee9
Glazkova_Marina <glazkova.mv@bk.ru>
2023-08-09 18:02:45 +0300

    deleteNote v1

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def readNotes():
         try:
             with open('notes.json', 'r') as file:
                 data = json.load(file)
+                flag = True
                 for i in data['note']:
+                    flag = False
                     print('ID:', i['id'])
                     print('Название заметки:', i['name'])
                     print('Содержание заметки:', i['body'])
@@ -68,22 +70,27 @@ def createNote():
             choise = 2
 
 def filterDate():
-    try:
-        with open('notes.json', 'r') as file:
-            data = findNote(3)
-            flag = True
-            for i in data:
-                flag = False
-                print('ID:', i['id'])
-                print('Название заметки:', i['name'])
-                print('Содержание заметки:', i['body'])
-                print('Дата создания:', i['date'])
-                print('Время создания:', i['time'])
-                print()
-            if flag:
-                print('Заметок в этот день не было!')
-    except OSError:
-        print('\nПроизошла ошибка при работе с файлом при фильтрации дат!\n')
+    if os.path.exists("notes.json"):
+        try:
+            with open('notes.json', 'r') as file:
+                data = findNote(3)
+                if data == None:
+                    return None
+                flag = True
+                for i in data:
+                    flag = False
+                    print('ID:', i['id'])
+                    print('Название заметки:', i['name'])
+                    print('Содержание заметки:', i['body'])
+                    print('Дата создания:', i['date'])
+                    print('Время создания:', i['time'])
+                    print()
+                if flag:
+                    print('Заметок в этот день не было!')
+        except OSError:
+            print('\nПроизошла ошибка при работе с файлом при фильтрации дат!\n')
+    else:
+        print('\nЗаметок пока еще не создавалось!\n')
 def findNote(choise):
         try:
             with open('notes.json', 'r') as file:
@@ -104,9 +111,21 @@ def findNote(choise):
                         if i.get('name') == choise_name:
                             return i
                 else:
-                    choise_day = int(input('Введите день: '))
-                    choise_month = int(input('Введите месяц: '))
-                    choise_year = int(input('Введите год: '))
+                    try:
+                        choise_day = int(input('Введите день: '))
+                    except ValueError:
+                        print('\nОшибка ввода. Необходимо ввести число!\n')
+                        return None
+                    try:
+                        choise_month = int(input('Введите месяц: '))
+                    except ValueError:
+                        print('\nОшибка ввода. Необходимо ввести число!\n')
+                        return None
+                    try:
+                        choise_year = int(input('Введите год: '))
+                    except ValueError:
+                        print('\nОшибка ввода. Необходимо ввести число!\n')
+                        return None
                     date = ''
                     if choise_day < 10:
                         date = date + '0' + str(choise_day) + '.'
@@ -162,6 +181,40 @@ def editNote():
     else:
         print('\nРедактировать нечего. Заметок пока еще не создавалось!\n')
 
+def deleteNote():
+    if os.path.exists("notes.json"):
+        choise = 0
+        while choise < 1 or choise > 2:
+            try:
+                choise = int(input('\nВы хотите найти заметку для удаления по: \n1 - ID,\n2 - названию?'))
+                if choise < 1 or choise > 2:
+                    print('\nОшибка ввода. Необходимо ввести число от 1 до 2!\n')
+            except ValueError:
+                print('\nОшибка ввода. Необходимо ввести число от 1 до 2!\n')
+            if choise > 0 and choise < 3:
+                note = findNote(choise)
+                if note == None:
+                    print('Заметки с таким именем нет!')
+                    break
+                try:
+                    data = {}
+                    with open('notes.json', 'r') as file:
+                        data = json.load(file)
+                        new_data = {}
+                        new_data['note'] = []
+                        j = 0
+                        for i in range(len(data['note'])):
+                            if data['note'][i]['id'] != note['id']:
+                                data['note'][i]['id'] = j
+                                new_data['note'].append(data['note'][i])
+                                j = j + 1
+                    with open('notes.json', 'w') as file:
+                        json.dump(new_data, file, indent='\t', ensure_ascii=False)
+                        print('Заметка удалена!')
+                except OSError:
+                    print('\nПроизошла ошибка при работе с файлом:(\n')
+    else:
+        print('\nУдалять нечего. Заметок пока еще не создавалось!\n')
 
 def main():
     choise = 0
@@ -176,6 +229,8 @@ def main():
             createNote()
         elif choise == 2:
             editNote()
+        elif choise == 3:
+            deleteNote()
         elif choise == 4:
             readNotes()
         elif choise == 5:
