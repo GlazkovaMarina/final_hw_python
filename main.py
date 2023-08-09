bce6175de02c548b8de6f70d7836d3e74a12f064
Glazkova_Marina <glazkova.mv@bk.ru>
2023-08-09 13:29:02 +0300

    task started

---
+++ main.py
@@ -0,0 +1,12 @@
+note = {
+    'id': 0,
+    'name': 'New note',
+    'body': 'test note',
+    'date': '05.05.2023',
+    'time': '12:35'
+}
+
+print(note.get('date'));
+
+def createNode():
+    print("Введите название: ")
